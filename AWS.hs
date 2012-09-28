83028175a7475137c061fcfeac086a2b1b7e990d
Yusuke Nomura <yunomu@gmail.com>
2012-09-28 10:19:45 +0900

    modify haddock comment

--- AWS.hs
+++ AWS.hs
@@ -1,3 +1,10 @@
+-- | aws-sdk is an AWS library for Haskell
+--
+-- Put your AWS AccessKey and SecretAccessKey into a configuration
+-- file. Write the following in /./\//aws.config/.
+--
+-- > accessKey: your-access-key
+-- > secretAccessKey: your-secret-access-key
 {-# LANGUAGE OverloadedStrings #-}
 module AWS
     ( -- * Credentials
