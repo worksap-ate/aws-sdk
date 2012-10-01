c728d357c27535fe739e6316e2cb0fbfdeeb434f
Yusuke Nomura <yunomu@gmail.com>
2012-10-01 10:40:16 +0900

    createvolume

--- AWS/EC2/Volume.hs
+++ AWS/EC2/Volume.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
-
 module AWS.EC2.Volume
     ( describeVolumes
+    , createVolume
     ) where
 
 import Data.Text (Text)
@@ -23,30 +23,56 @@ describeVolumes
     -> [Filter] -- ^ Filters
     -> EC2 m (Source m Volume)
 describeVolumes vids filters =
-    ec2QuerySource "DescribeVolumes" params volumeSet
+    ec2QuerySource "DescribeVolumes" params $
+        itemConduit "volumeSet" volumeSink
   where
     params =
         [ ArrayParams "VolumeId" vids
         , FilterParams filters
         ]
-    volumeSet :: MonadThrow m
-        => GLConduit Event m Volume
-    volumeSet = itemConduit "volumeSet" $
-        Volume
+
+volumeSink :: MonadThrow m
+    => GLSink Event m Volume
+volumeSink = Volume
+    <$> getT "volumeId"
+    <*> getF "size" textToInt
+    <*> getMT "snapshotId"
+    <*> getT "availabilityZone"
+    <*> getF "status" volumeStatus
+    <*> getF "createTime" textToTime
+    <*> itemsSet "attachmentSet" (
+        attachment
         <$> getT "volumeId"
-        <*> getF "size" textToInt
-        <*> getMT "snapshotId"
-        <*> getT "availabilityZone"
-        <*> getF "status" volumeStatus
-        <*> getF "createTime" textToTime
-        <*> itemsSet "attachmentSet" (
-            Attachment
-            <$> getT "volumeId"
-            <*> getT "instanceId"
-            <*> getT "device"
-            <*> getF "status" attachmentStatus
-            <*> getF "attachTime" textToTime
-            <*> getF "deleteOnTermination" textToBool
-            )
-        <*> resourceTagSink
-        <*> volumeTypeSink
+        <*> getT "instanceId"
+        <*> getT "device"
+        <*> getF "status" attachmentStatus
+        <*> getF "attachTime" textToTime
+        <*> getF "deleteOnTermination" textToBool
+        )
+    <*> resourceTagSink
+    <*> volumeTypeSink
+
+volumeTypeParam :: VolumeType -> [QueryParam]
+volumeTypeParam Standard = [ValueParam "VolumeType" "standard"]
+volumeTypeParam (IO1 iops) =
+    [ ValueParam "VolumeType" "io1"
+    , ValueParam "Iops" $ toText iops
+    ]
+
+createVolume
+    :: (MonadResource m, MonadBaseControl IO m)
+    => Maybe Int -- ^ Size
+    -> Maybe Text -- ^ SnapshotId
+    -> Maybe Text -- ^ AvailabilityZone
+    -> Maybe VolumeType
+    -> EC2 m Volume
+createVolume size sid zone vtype =
+    ec2Query "CreateVolume" params volumeSink
+  where
+    f name = maybe [] (\a -> [ValueParam name a])
+    params = maybe [] volumeTypeParam vtype
+        ++ (uncurry f =<<
+            [ ("Size", toText <$> size)
+            , ("SnapshotId", sid)
+            , ("AvailabilityZone", zone)
+            ])
