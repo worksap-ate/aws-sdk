4c3f99b0586fab75c3eaaf2332448f2af1fe0aab
Yusuke Nomura <yunomu@gmail.com>
2012-10-22 19:56:17 +0900

    Add wait utility

--- AWS/EC2/Util.hs
+++ AWS/EC2/Util.hs
@@ -4,12 +4,19 @@ module AWS.EC2.Util
     ( asList
     , head
     , each
+    , wait
     ) where
 
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import Control.Monad.Trans.Class (lift)
 import Prelude hiding (head)
+import Safe
+import qualified Control.Concurrent as CC
+import Control.Monad.IO.Class (liftIO, MonadIO)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Control.Applicative
 
 import AWS.EC2.Internal
 
@@ -49,3 +56,31 @@ each' rsrc f = do
     case ma of
         Nothing -> return ()
         Just a  -> f a >> each' s' f
+
+-- | Wait for condition.
+--
+-- > import AWS.EC2
+-- > import AWS.EC2.Types
+-- > import AWS.EC2.Util (asList, wait)
+-- > 
+-- > waitForAvailable :: (MonadIO m, Functor m)
+-- >     => Text -- ^ ImageId
+-- >     -> EC2 m a
+-- > waitForAvailable = wait
+-- >     (\img -> imageImageState img == ImageAvailable)
+-- >     (\imgId -> asList (describeImages [imgId] [] [] []))
+wait
+    :: (MonadIO m, Functor m)
+    => (a -> Bool) -- ^ condition
+    -> (Text -> EC2 m [a]) -- ^ DescribeResources
+    -> Text -- ^ Resource Id
+    -> EC2 m a
+wait f g rid = do
+    mr <- headMay <$> g rid
+    case mr of
+        Nothing -> fail $ "Resource not found: " ++ T.unpack rid
+        Just r  -> if f r
+            then return r
+            else do
+                liftIO $ CC.threadDelay 5
+                wait f g rid
