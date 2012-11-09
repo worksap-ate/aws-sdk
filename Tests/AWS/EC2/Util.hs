ec9ccda23f07d88f3c7599d9921b3ca2fcfe2562
daimatz <dai@daimatz.net>
2012-11-10 05:29:04 +0900

    add test framework

---
+++ Tests/AWS/EC2/Util.hs
@@ -0,0 +1,27 @@
+module Tests.AWS.EC2.Util
+    ( testEC2
+    , testEC2'
+    )
+    where
+
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import Control.Monad.Trans.Class (lift)
+
+import AWS
+import AWS.EC2
+
+testEC2 region request = do
+    cred <- loadCredential
+    runResourceT $ do
+        runEC2 cred $ do
+            setRegion region
+            response <- request
+            lift $ response $$+- CL.consume
+
+testEC2' region request = do
+    cred <- loadCredential
+    runResourceT $ do
+        runEC2 cred $ do
+            setRegion region
+            request
