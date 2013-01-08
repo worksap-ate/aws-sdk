41bb6f5b5bb22e988c71d22fba20bded102feb70
amkkun <amkkun@gmail.com>
2013-01-08 18:02:13 +0900

    Add DescribeSnapshotAttributeTest.

--- Tests/AWSTests/EC2Tests/SnapshotTests.hs
+++ Tests/AWSTests/EC2Tests/SnapshotTests.hs
@@ -9,6 +9,7 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.EC2
+import AWS.EC2.Types
 import AWSTests.Util
 import AWSTests.EC2Tests.Util
 
@@ -18,9 +19,18 @@ region = "ap-northeast-1"
 runSnapshotTests :: IO ()
 runSnapshotTests = do
     hspec describeSnapshotsTest
+    hspec describeSnapshotAttributeTest
 
 describeSnapshotsTest :: Spec
 describeSnapshotsTest = do
     describe "describeSnapshots doesn't fail" $ do
         it "describeSnapshots doesn't throw any exception" $ do
             testEC2 region (describeSnapshots [] [] [] []) `miss` anyHttpException
+
+describeSnapshotAttributeTest :: Spec
+describeSnapshotAttributeTest = do
+    describe "describeSnapshotAttribute doesn't fail" $ do
+        it "describeSnapshotAttribute doesn't throw any exception" $ do
+            snapshots <- testEC2 region (describeSnapshots [] [] [] [])
+            let sid = snapshotId $ head snapshots
+            testEC2' region (describeSnapshotAttribute sid SnapshotAttributeRequestCreateVolumePermission) `miss` anyHttpException
