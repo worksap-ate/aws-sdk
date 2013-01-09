253535d1006d5d4c04c7403ad53c032f9b475762
amkkun <amkkun@gmail.com>
2013-01-09 14:39:09 +0900

    Add rdsQueryDebug.

--- AWS/RDS/Internal.hs
+++ AWS/RDS/Internal.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, CPP #-}
 
 module AWS.RDS.Internal
     where
@@ -13,6 +13,14 @@ import Data.XML.Types (Event(..))
 import AWS.Class
 import AWS.Lib.Query
 import AWS.Lib.Parser
+
+#ifdef DEBUG
+import Control.Monad.IO.Class (liftIO)
+import qualified Control.Monad.Reader as Reader
+import qualified Control.Monad.State as State
+import Control.Monad.Trans.Class (lift)
+import qualified Data.Conduit.Binary as CB
+#endif
 
 apiVersion :: ByteString
 apiVersion = "2012-09-17"
@@ -32,3 +40,19 @@ elements :: MonadThrow m
     -> GLSink Event m a
     -> GLSink Event m [a]
 elements name f = element (name <> "s") $ listConsumer name f
+
+#ifdef DEBUG
+rdsQueryDebug
+    :: (MonadResource m, MonadBaseControl IO m)
+    => ByteString
+    -> [QueryParam]
+    -> RDS m a
+rdsQueryDebug action params = do
+    cred <- Reader.ask
+    ctx <- State.get
+    lift $ do
+        liftIO $ mapM_ putStrLn $ showUnitParams params
+        response <- requestQuery cred ctx action params apiVersion sinkError
+        (res, _) <- unwrapResumable response
+        res $$ CB.sinkFile "debug.txt" >>= fail "debug"
+#endif
