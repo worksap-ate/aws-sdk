535de4b9938aa1814af303359452b9689c9a5b26
eagletmt <eagletmt@gmail.com>
2013-01-17 19:22:39 +0900

    Add a test case for {Create,Delete}PlacementGroup

--- Tests/AWSTests/EC2Tests/PlacementGroupTests.hs
+++ Tests/AWSTests/EC2Tests/PlacementGroupTests.hs
@@ -7,6 +7,7 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.EC2
+import AWS.EC2.Types (PlacementGroupStrategy(..))
 import AWSTests.Util
 import AWSTests.EC2Tests.Util
 
@@ -16,9 +17,21 @@ region = "us-east-1"
 runPlacementGroupTests :: IO ()
 runPlacementGroupTests = do
     hspec describePlacementGroupsTest
+    hspec createAndDeletePlacementGroupTest
 
 describePlacementGroupsTest :: Spec
 describePlacementGroupsTest = do
     describe "describePlacementGroups doesn't fail" $ do
         it "describeInstances doesn't throw any exception" $ do
             testEC2 region (describePlacementGroups [] []) `miss` anyHttpException
+
+createAndDeletePlacementGroupTest :: Spec
+createAndDeletePlacementGroupTest = do
+    describe "{create,delete}PlacementGroup doesn't fail" $ do
+        it "{create,delete}PlacementGroup doesn't throw any exception" $ do
+            testEC2' region (do
+                createPlacementGroup groupName PlacementGroupStrategyCluster
+                deletePlacementGroup groupName
+                ) `miss` anyHttpException
+  where
+    groupName = "createAndDeletePlacementGroupTest"
