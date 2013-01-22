7cba80c40a64dbaaf69ee57a56c4835f9c323cda
amkkun <amkkun@gmail.com>
2013-01-22 18:20:16 +0900

    Add a test case for ImportVolume.

--- Tests/AWSTests/EC2Tests/ConversionTaskTests.hs
+++ Tests/AWSTests/EC2Tests/ConversionTaskTests.hs
@@ -9,6 +9,7 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.EC2
+import AWS.EC2.Types (ConversionTask(..), ImportVolumeRequestImage(..))
 import AWSTests.Util
 import AWSTests.EC2Tests.Util
 
@@ -18,9 +19,25 @@ region = "ap-northeast-1"
 runConversionTaskTests :: IO ()
 runConversionTaskTests = do
     hspec describeConversionTasksTest
+    hspec importVolumeTest
 
 describeConversionTasksTest :: Spec
 describeConversionTasksTest = do
     describe "describeConversionTasks doesn't fail" $ do
         it "describeConversionTasks doesn't throw any exception" $ do
             testEC2 region (describeConversionTasks []) `miss` anyHttpException
+
+importVolumeTest :: Spec
+importVolumeTest = do
+    describe "importVolume doesn't fail" $ do
+        it "importVolume doesn't throw any exception" $ do
+            task <- testEC2' region test
+            let taskId = conversionTaskId task
+            testEC2' region (cancelConversionTask taskId)
+                `shouldReturn` True
+  where
+    test = importVolume
+        "ap-northeast-1a"
+        (ImportVolumeRequestImage "RAW" 8 "https://test.test")
+        Nothing
+        8
