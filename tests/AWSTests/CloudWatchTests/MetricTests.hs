576c2c2186642d45ce9f4e665f82d32c4f04dca2
eagletmt <eagletmt@gmail.com>
2013-02-05 18:13:36 +0900

    Add a test case for GetMetricStatistics

--- tests/AWSTests/CloudWatchTests/MetricTests.hs
+++ tests/AWSTests/CloudWatchTests/MetricTests.hs
@@ -3,10 +3,13 @@ module AWSTests.CloudWatchTests.MetricTests
     )
     where
 
+import Control.Monad.IO.Class (liftIO)
 import Data.Text (Text)
+import Data.Time (UTCTime(..), getCurrentTime)
 import Test.Hspec
 
 import AWS.CloudWatch
+import AWS.CloudWatch.Types
 import AWSTests.Util
 import AWSTests.CloudWatchTests.Util
 
@@ -22,3 +25,11 @@ listMetricsTest = do
     describe "Metric operations doesn't fail" $ do
         it "listMetrics doesn't throw any exception" $ do
             testCloudWatch region (listMetrics [] Nothing Nothing Nothing) `miss` anyHttpException
+
+        it "getMetricStatistics doesn't throw any exception" $ do
+            testCloudWatch region (do
+                (metric:_, _) <- listMetrics [] Nothing Nothing Nothing
+                end <- liftIO getCurrentTime
+                let start = end { utctDayTime = utctDayTime end - 5*60 }
+                getMetricStatistics [] start end (metricName metric) (metricNameSpace metric) 60 allStatistics Nothing
+                ) `miss` anyHttpException
