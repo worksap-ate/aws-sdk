4482f2b66bf617689702003e7c3cc6a8d47f7a66
amkkun <amkkun@gmail.com>
2013-02-12 17:20:29 +0900

    Add a test case for {create,delete}DBParameterGroup.

--- tests/AWSTests/RDSTests/DBParameterGroupTests.hs
+++ tests/AWSTests/RDSTests/DBParameterGroupTests.hs
@@ -16,6 +16,7 @@ region = "ap-northeast-1"
 runDBParameterGroupTests :: IO ()
 runDBParameterGroupTests = do
     hspec describeDBParameterGroupsTest
+    hspec createAndDeleteDBParameterGroupTest
 
 describeDBParameterGroupsTest :: Spec
 describeDBParameterGroupsTest = do
@@ -23,3 +24,14 @@ describeDBParameterGroupsTest = do
         it "describeDBParameterGroups doesn't throw any exception" $ do
             testRDS region (describeDBParameterGroups Nothing Nothing Nothing)
                 `miss` anyHttpException
+
+createAndDeleteDBParameterGroupTest :: Spec
+createAndDeleteDBParameterGroupTest = do
+    describe "{create,delete}DBParameterGroup doesn't fail" $ do
+        it "{create,delete}DBParameterGroup doesn't throw any exception" $ do
+            testRDS region (do
+                createDBParameterGroup "MySQL5.5" name "test"
+                deleteDBParameterGroup name
+                ) `miss` anyHttpException
+  where
+    name = "hspec-test-parameter-group"
