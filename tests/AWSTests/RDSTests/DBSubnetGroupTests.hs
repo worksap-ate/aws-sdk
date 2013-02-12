7839ac885cee3c5cac947ebf47852f216d616d06
amkkun <amkkun@gmail.com>
2013-02-12 18:43:32 +0900

    Add a test case for RDS/{create,delete}DBSubnetGroup.

--- tests/AWSTests/RDSTests/DBSubnetGroupTests.hs
+++ tests/AWSTests/RDSTests/DBSubnetGroupTests.hs
@@ -7,6 +7,7 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.RDS
+import AWS.RDS.Types (DBSubnetGroup(..), Subnet(..))
 import AWSTests.Util
 import AWSTests.RDSTests.Util
 
@@ -16,9 +17,24 @@ region = "ap-northeast-1"
 runDBSubnetGroupTests :: IO ()
 runDBSubnetGroupTests = do
     hspec describeDBSubnetGroupsTest
+    hspec createAndDeleteDBSubnetGroupTest
 
 describeDBSubnetGroupsTest :: Spec
 describeDBSubnetGroupsTest = do
     describe "describeDBSubnetGroups doesn't fail" $ do
         it "describeDBSubnetGroups doesn't throw any exception" $ do
-            testRDS region (describeDBSubnetGroups Nothing Nothing Nothing) `miss` anyHttpException
+            testRDS region (describeDBSubnetGroups Nothing Nothing Nothing)
+                `miss` anyHttpException
+
+createAndDeleteDBSubnetGroupTest :: Spec
+createAndDeleteDBSubnetGroupTest = do
+    describe "{create,delete}DBSubnetGroup doesn't fail" $ do
+        it "{create,delete}DBSubnetGroup doesn't throw any exception" $ do
+            testRDS region (do
+                sgs <- describeDBSubnetGroups Nothing Nothing Nothing
+                createDBSubnetGroup name (subnets sgs) "test"
+                deleteDBSubnetGroup name
+                ) `miss` anyHttpException
+  where
+    name = "hspec-test-subnet-group"
+    subnets = map subnetIdentifier . dbsngSubnets . head
