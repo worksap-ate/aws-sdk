8c7c4bf90c45e940fb0564d30e256e73484fe56e
amkkun <amkkun@gmail.com>
2013-02-12 18:13:19 +0900

    Add RDS/CreateDBSubnetGroup.

--- AWS/RDS/DBSubnetGroup.hs
+++ AWS/RDS/DBSubnetGroup.hs
@@ -2,13 +2,15 @@
 
 module AWS.RDS.DBSubnetGroup
     ( describeDBSubnetGroups
+    , createDBSubnetGroup
     ) where
 
 import Control.Applicative ((<$>))
 import Data.Conduit
 import Data.Text (Text)
 
-import AWS.Lib.Query ((|=?))
+import AWS.Lib.Parser (element)
+import AWS.Lib.Query ((|=), (|=?), (|.#=))
 import AWS.RDS.Internal
 import AWS.RDS.Types (DBSubnetGroup)
 import AWS.Util (toText)
@@ -27,4 +29,20 @@ describeDBSubnetGroups name marker maxRecords =
         [ "DBSubnetGroupName" |=? name
         , "Marker" |=? marker
         , "MaxRecords" |=? toText <$> maxRecords
+        ]
+
+createDBSubnetGroup
+    :: (MonadBaseControl IO m, MonadResource m)
+    => Text -- ^ DBSubnetGroupName
+    -> [Text] -- ^ SubnetIds
+    -> Text -- ^ DBSubnetGroupDescription
+    -> RDS m DBSubnetGroup
+createDBSubnetGroup name ids desc =
+    rdsQuery "CreateDBSubnetGroup" params $
+        element "DBSubnetGroup" dbSubnetGroupSink
+  where
+    params =
+        [ "DBSubnetGroupName" |= name
+        , "SubnetIds.member" |.#= ids
+        , "DBSubnetGroupDescription" |= desc
         ]
