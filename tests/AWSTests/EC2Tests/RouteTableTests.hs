3371dad1e0c01a03d5c453a421629b93dc7d85ce
eagletmt <eagletmt@gmail.com>
2013-02-20 16:43:36 +0900

    Add test cases for {create,delete}RouteTable and [dis]associateRouteTable

--- tests/AWSTests/EC2Tests/RouteTableTests.hs
+++ tests/AWSTests/EC2Tests/RouteTableTests.hs
@@ -9,6 +9,7 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.EC2
+import AWS.EC2.Types
 import AWSTests.Util
 import AWSTests.EC2Tests.Util
 
@@ -16,11 +17,33 @@ region :: Text
 region = "ap-northeast-1"
 
 runRouteTableTests :: IO ()
-runRouteTableTests = do
-    hspec describeRouteTablesTest
+runRouteTableTests = hspec $ do
+    describeRouteTablesTest
+    createAndDeleteRouteTableTest
+    associateAndDisassociateRouteTableTest
 
 describeRouteTablesTest :: Spec
 describeRouteTablesTest = do
-    describe "describeRouteTables doesn't fail" $ do
-        it "describeRouteTables doesn't throw any exception" $ do
+    describe "describeRouteTables" $ do
+        it "doesn't throw any exception" $ do
             testEC2 region (describeRouteTables [] []) `miss` anyConnectionException
+
+createAndDeleteRouteTableTest :: Spec
+createAndDeleteRouteTableTest = do
+    describe "{create,delete}RouteTable" $ do
+        it "doesn't throw any exception" $ do
+            testEC2' region (
+                withVpc "10.0.0.0/24" $ \Vpc{vpcId = vpc} ->
+                    withRouteTable vpc $ const (return ())
+                ) `miss` anyConnectionException
+
+associateAndDisassociateRouteTableTest :: Spec
+associateAndDisassociateRouteTableTest = do
+    describe "[dis]associateRouteTable" $ do
+        it "doesn't throw any exception" $ do
+            testEC2' region (
+                withSubnet "10.0.0.0/24" $ \Subnet{subnetId = subnet, subnetVpcId = vpc} ->
+                    withRouteTable vpc $ \RouteTable{routeTableId = routeTable} -> do
+                        association <- associateRouteTable routeTable subnet
+                        disassociateRouteTable association
+                ) `miss` anyConnectionException
