70ebe2146b809c1b618cc9bf429556779cf3a243
eagletmt <eagletmt@gmail.com>
2013-02-20 17:20:02 +0900

    Add a test case for {create,delete}Tags

--- tests/AWSTests/EC2Tests/TagTests.hs
+++ tests/AWSTests/EC2Tests/TagTests.hs
@@ -9,6 +9,8 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.EC2
+import AWS.EC2.Types
+import qualified AWS.EC2.Util as Util
 import AWSTests.Util
 import AWSTests.EC2Tests.Util
 
@@ -16,11 +18,26 @@ region :: Text
 region = "ap-northeast-1"
 
 runTagTests :: IO ()
-runTagTests = do
-    hspec describeTagsTest
+runTagTests = hspec $ do
+    describeTagsTest
+    createAndDeleteTagsTest
 
 describeTagsTest :: Spec
 describeTagsTest = do
-    describe "describeTags doesn't fail" $ do
-        it "describeTags doesn't throw any exception" $ do
+    describe "describeTags" $ do
+        it "doesn't throw any exception" $ do
             testEC2 region (describeTags []) `miss` anyConnectionException
+
+createAndDeleteTagsTest :: Spec
+createAndDeleteTagsTest = do
+    describe "{create,delete}Tags" $ do
+        it "doesn't throw any exception" $ do
+            testEC2' region (do
+                Image{imageId = image}:_ <- Util.list $ describeImages [] [] [] []
+                SecurityGroup{securityGroupId = sg}:_ <- Util.list $ describeSecurityGroups [] [] []
+                let ids = [image, sg]
+                    tags = [("createTagsTest", "tag1"), ("deleteTagsTest", "tag2")]
+                    rtags = map (\(k,v) -> ResourceTag k (Just v)) tags
+                createTags ids tags
+                deleteTags ids rtags
+                ) `miss` anyConnectionException
