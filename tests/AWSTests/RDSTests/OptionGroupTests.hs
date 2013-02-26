10b22311448091b4268114ce5ad92be2dc293020
amkkun <amkkun@gmail.com>
2013-02-26 17:21:57 +0900

    Add a test case for {create,delete}OptionGroup.

--- tests/AWSTests/RDSTests/OptionGroupTests.hs
+++ tests/AWSTests/RDSTests/OptionGroupTests.hs
@@ -16,6 +16,7 @@ region = "ap-northeast-1"
 runOptionGroupTests :: IO ()
 runOptionGroupTests = hspec $ do
     describeOptionGroupsTest
+    createAndDeleteOptionGroupTest
 
 describeOptionGroupsTest :: Spec
 describeOptionGroupsTest = do
@@ -24,3 +25,14 @@ describeOptionGroupsTest = do
             testRDS region (
                 describeOptionGroups Nothing Nothing Nothing Nothing Nothing
                 ) `miss` anyConnectionException
+
+createAndDeleteOptionGroupTest :: Spec
+createAndDeleteOptionGroupTest = do
+    describe "{create,delete}OptionGroup doesn't fail" $ do
+        it "{create,delete}OptionGroup doesn't throw any exception" $ do
+            testRDS region (do
+                createOptionGroup "MySQL" "5.5" "hspec-test" name
+                deleteOptionGroup name
+                ) `miss` anyConnectionException
+  where
+    name = "hspec-test-option-group"
