9220d7c050b83bd6f38021ad19f4cd5acaffaa5a
amkkun <amkkun@gmail.com>
2013-02-26 12:50:29 +0900

    Add a test case for RDS/CopyDBSnapshot.

--- tests/AWSTests/RDSTests/DBSnapshotTests.hs
+++ tests/AWSTests/RDSTests/DBSnapshotTests.hs
@@ -6,6 +6,7 @@ module AWSTests.RDSTests.DBSnapshotTests
     where
 
 import Control.Applicative ((<$>))
+import Control.Monad.IO.Class (liftIO)
 import Data.Conduit
 import Data.Text (Text)
 import Test.Hspec
@@ -20,9 +21,10 @@ region :: Text
 region = "ap-northeast-1"
 
 runDBSnapshotTests :: IO ()
-runDBSnapshotTests = do
-    hspec describeDBSnapshotsTest
-    hspec createDBSnapshotTest
+runDBSnapshotTests = hspec $ do
+    describeDBSnapshotsTest
+    createDBSnapshotTest
+    copyDBSnapshotTest
 
 describeDBSnapshotsTest :: Spec
 describeDBSnapshotsTest = do
@@ -36,8 +38,8 @@ createDBSnapshotTest :: Spec
 createDBSnapshotTest = do
     describe "{create,delete}DBSnapshot doesn't fail" $ do
         it "{create,delete}DBSnapshot doesn't throw any exception" $ do
-            dbsid <- getRandomText "hspec-create-delete-"
             testRDS region (do
+                dbsid <- liftIO $ getRandomText "hspec-create-delete-"
                 dbiid <- dbInstanceIdentifier . head <$>
                     describeDBInstances Nothing Nothing Nothing
                 withDBSnapshot dbiid dbsid $
@@ -48,3 +50,16 @@ waitUntilAvailable :: (MonadBaseControl IO m, MonadResource m) => Text -> RDS m 
 waitUntilAvailable = wait
     (\dbs -> dbSnapshotStatus dbs == "available")
     (\dbsid -> describeDBSnapshots Nothing (Just dbsid) Nothing Nothing Nothing)
+
+copyDBSnapshotTest :: Spec
+copyDBSnapshotTest = do
+    describe "copyDBSnapshot doesn't fail" $ do
+        it "copyDBSnapshot doesn't throw any exception" $ do
+            testRDS region (do
+                target <- liftIO $ getRandomText "hspec-copy-"
+                source <- dbSnapshotIdentifier . head <$>
+                    describeDBSnapshots Nothing Nothing Nothing Nothing (Just "automated")
+                copyDBSnapshot source target
+                waitUntilAvailable target
+                deleteDBSnapshot target
+                ) `miss` anyConnectionException
