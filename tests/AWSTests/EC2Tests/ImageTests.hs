2fde4f2038901d477e80d975d44f687f08c73161
eagletmt <eagletmt@gmail.com>
2013-02-26 16:22:17 +0900

    Add a test case for {create,deregister}Image

--- tests/AWSTests/EC2Tests/ImageTests.hs
+++ tests/AWSTests/EC2Tests/ImageTests.hs
@@ -9,6 +9,7 @@ import Data.Text (Text)
 import Test.Hspec
 
 import AWS.EC2
+import AWS.EC2.Types
 import AWSTests.Util
 import AWSTests.EC2Tests.Util
 
@@ -16,11 +17,18 @@ region :: Text
 region = "ap-northeast-1"
 
 runImageTests :: IO ()
-runImageTests = do
-    hspec describeImageTest
-
-describeImageTest :: Spec
-describeImageTest = do
-    describe "describeImages doesn't fail" $ do
-        it "describeImages doesn't throw any exception" $ do
+runImageTests = hspec $ do
+    describe "describeImages" $ do
+        it "doesn't throw any exception" $ do
             testEC2 region (describeImages [] [] [] []) `miss` anyConnectionException
+
+    describe "{create,deregister}Image" $ do
+        it "doesn't throw any exception" $ do
+            testEC2' region (
+                withInstance testRunInstancesRequest $ \Instance{instanceId = inst} -> do
+                    waitForInstanceState InstanceStateRunning inst
+                    let name = "createImageTest"
+                        desc = "For HSpec testing"
+                    ami <- createImage inst name (Just desc) False []
+                    deregisterImage ami
+                ) `miss` anyConnectionException
