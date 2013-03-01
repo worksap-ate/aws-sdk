6e891fc28a64d4b7644353970376c47f2db76a70
amkkun <amkkun@gmail.com>
2013-03-01 15:02:57 +0900

    Add a test case ofor {modify,reset}DBParameterGroup.

--- tests/AWSTests/RDSTests/DBParameterGroupTests.hs
+++ tests/AWSTests/RDSTests/DBParameterGroupTests.hs
@@ -4,6 +4,8 @@ module AWSTests.RDSTests.DBParameterGroupTests
     where
 
 import Control.Applicative ((<$>))
+import qualified Control.Concurrent as CC
+import Control.Monad.IO.Class (liftIO)
 import Data.Text (Text)
 import Test.Hspec
 
@@ -20,6 +22,7 @@ runDBParameterGroupTests = hspec $ do
     describeDBParameterGroupsTest
     createAndDeleteDBParameterGroupTest
     describeDBParametersTest
+    modifyAndResetDBParameterGroupTest
 
 describeDBParameterGroupsTest :: Spec
 describeDBParameterGroupsTest = do
@@ -33,10 +36,9 @@ createAndDeleteDBParameterGroupTest = do
     describe "{create,delete}DBParameterGroup doesn't fail" $ do
         it "{create,delete}DBParameterGroup doesn't throw any exception" $ do
             testRDS region (do
+                name <- liftIO $ getRandomText "hspec-test-parameter-group-"
                 withDBParameterGroup name $ const $ return ()
                 ) `miss` anyConnectionException
-  where
-    name = "hspec-test-parameter-group"
 
 describeDBParametersTest :: Spec
 describeDBParametersTest = do
@@ -51,3 +53,23 @@ describeDBParametersTest = do
     loop name marker = do
         (marker', _) <- describeDBParameters name marker (Just 100) Nothing
         maybe (return ()) (\m -> loop name (Just m)) marker'
+
+modifyAndResetDBParameterGroupTest :: Spec
+modifyAndResetDBParameterGroupTest = do
+    describe "{modify,reset}DBParameterGroup doesn't fail" $ do
+        it "{modify,reset}DBParameterGroup doesn't throw any exception" $ do
+            testRDS region (do
+                name <- liftIO $ getRandomText "hspec-test-parameter-group-"
+                withDBParameterGroup name $ \_ -> do
+                    modifyDBParameterGroup name modifyParams
+                    liftIO $ CC.threadDelay 10000000
+                    resetDBParameterGroup name resetParams
+                    resetDBParameterGroup name ResetAllParameters
+                ) `miss` anyConnectionException
+  where
+    modifyParams =
+        [ ModifyParameter "auto_increment_increment" "2" ApplyMethodImmediate
+        ]
+    resetParams = ResetParameters
+        [ ResetParameter "auto_increment_increment" ApplyMethodImmediate
+        ]
