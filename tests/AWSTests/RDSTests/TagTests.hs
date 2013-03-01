839d693714d57f3a4016cf08d66d17363b42a5b5
amkkun <amkkun@gmail.com>
2013-03-01 18:49:56 +0900

    Add a test case for {add,remove}Tags{To,From}Resource.

--- tests/AWSTests/RDSTests/TagTests.hs
+++ tests/AWSTests/RDSTests/TagTests.hs
@@ -19,6 +19,7 @@ region = "ap-northeast-1"
 runRDSTagTests :: IO ()
 runRDSTagTests = hspec $ do
     listTagsForResourceTest
+    addAndRemoveTagsTest
 
 listTagsForResourceTest :: Spec
 listTagsForResourceTest = do
@@ -27,7 +28,27 @@ listTagsForResourceTest = do
             testRDS region (do
                 dbiid <- dbInstanceIdentifier . head <$>
                     describeDBInstances Nothing Nothing Nothing
-                listTagsForResource $ arn <> dbiid
+                listTagsForResource $ arn dbiid
                 ) `miss` anyConnectionException
-  where
-    arn = "arn:aws:rds:" <> region <> ":049669284607:db:"
+
+arn :: Text -> Text
+arn dbiid = "arn:aws:rds:" <> region <> ":049669284607:db:" <> dbiid
+
+addAndRemoveTagsTest :: Spec
+addAndRemoveTagsTest = do
+    describe "{add,remove}Tags{To,From}Resouce doesn't fail" $ do
+        it "{add,remove}Tags{To,From}Resouce doesn't throw any exception" $ do
+            testRDS region (do
+                dbiid <- dbInstanceIdentifier . head <$>
+                    describeDBInstances Nothing Nothing Nothing
+                let tags =
+                        [ Tag { tagKey = "hspec-test1"
+                              , tagValue =  "test1"
+                              }
+                        , Tag { tagKey = "hspec-test2"
+                              , tagValue = "test2"
+                              }
+                        ]
+                addTagsToResource (arn dbiid) tags
+                removeTagsFromResource (arn dbiid) $ map tagKey tags
+                ) `miss` anyConnectionException
