90a55a743a2a8eccb239dc13f3b7f7ca4ee0e193
amutake <amutake.s@gmail.com>
2013-12-11 18:47:08 +0900

    Remove unused functions

--- Cloud/AWS/EC2/Internal.hs
+++ Cloud/AWS/EC2/Internal.hs
@@ -7,15 +7,6 @@ module Cloud.AWS.EC2.Internal
     , initialEC2Context
     , runEC2
     , runEC2withManager
-    , itemConduit
-    , itemsSet
-    , resourceTagSink
-    , productCodeSink
-    , stateReasonSink
-    , volumeTypeSink
-    , groupSetSink
-    , networkInterfaceAttachmentSink
-      -- new parsers
     , itemConduit'
     , itemsSet'
     , resourceTagConv
@@ -28,18 +19,14 @@ module Cloud.AWS.EC2.Internal
 
 import Control.Monad (join)
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Trans.Class (lift)
 import qualified Network.HTTP.Conduit as HTTP
 import Data.ByteString.Char8 ()
 import Control.Applicative
 import Data.Conduit
-import qualified Data.Conduit.List as CL
-import Data.Maybe (fromMaybe)
 import Data.XML.Types (Event)
 import Data.Text (Text)
 
 import Cloud.AWS.Class
-import Cloud.AWS.Lib.Parser
 import Cloud.AWS.Lib.Parser.Unordered hiding (getT)
 import Cloud.AWS.EC2.Types
 
@@ -60,25 +47,12 @@ runEC2withManager :: Monad m
 runEC2withManager mgr =
     runAWSwithManager mgr initialEC2Context
 
-itemConduit :: MonadThrow m
-    => Text
-    -> Consumer Event m o
-    -> Conduit Event m o
-itemConduit tag inner =
-    fromMaybe (()) <$> elementM tag (listConduit "item" inner)
-
 itemConduit' :: (MonadThrow m, Applicative m)
     => Text
     -> (SimpleXML -> m o)
     -> Conduit Event m o
 itemConduit' tag inner = xmlParserConduit tag $ \xml ->
     getElement xml "item" inner
-
-itemsSet :: MonadThrow m
-    => Text
-    -> Consumer Event m o
-    -> Consumer Event m [o]
-itemsSet tag inner = itemConduit tag inner =$= CL.consume
 
 itemsSet' :: (MonadThrow m, Applicative m)
     => SimpleXML
@@ -87,55 +61,10 @@ itemsSet' :: (MonadThrow m, Applicative m)
     -> m [o]
 itemsSet' xml tag inner = getElements xml tag "item" inner
 
-resourceTagSink :: MonadThrow m
-    => Consumer Event m [ResourceTag]
-resourceTagSink = itemsSet "tagSet" $
-    ResourceTag
-    <$> getT "key"
-    <*> getT "value"
-
-productCodeSink :: MonadThrow m
-    => Consumer Event m [ProductCode]
-productCodeSink = itemsSet "productCodes" $
-    ProductCode
-    <$> getT "productCode"
-    <*> getT "type"
-
-stateReasonSink :: MonadThrow m
-    => Consumer Event m (Maybe StateReason)
-stateReasonSink = elementM "stateReason" $
-    StateReason
-    <$> getT "code"
-    <*> getT "message"
-
 volumeType :: MonadThrow m => Text -> Maybe Int -> m VolumeType
 volumeType t Nothing  | t == "standard" = return $ VolumeTypeStandard
 volumeType t (Just i) | t == "io1"      = return $ VolumeTypeIO1 i
 volumeType t _ = monadThrow $ FromTextError t
-
-volumeTypeSink :: MonadThrow m
-    => Consumer Event m VolumeType
-volumeTypeSink = volumeType <$> getT "volumeType" <*> getT "iops" >>= lift
-
-groupSetSink :: MonadThrow m => Consumer Event m [Group]
-groupSetSink = itemsSet "groupSet" $ Group
-    <$> getT "groupId"
-    <*> getT "groupName"
-
-networkInterfaceAttachmentSink
-    :: MonadThrow m
-    => Consumer Event m (Maybe NetworkInterfaceAttachment)
-networkInterfaceAttachmentSink = elementM "attachment" $
-    NetworkInterfaceAttachment
-    <$> getT "attachmentId"
-    <*> getT "instanceId"
-    <*> getT "instanceOwnerId"
-    <*> getT "deviceIndex"
-    <*> getT "status"
-    <*> getT "attachTime"
-    <*> getT "deleteOnTermination"
-
--- new parsers
 
 resourceTagConv :: (MonadThrow m, Applicative m)
     => SimpleXML -> m [ResourceTag]
