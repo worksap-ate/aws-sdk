cea8bad7e7ecd74994d54bf7b8f2234ec227be8b
amutake <amutake.s@gmail.com>
2013-12-11 16:34:58 +0900

    Change xml parsers in EC2/Snapshot

--- Cloud/AWS/EC2/Snapshot.hs
+++ Cloud/AWS/EC2/Snapshot.hs
@@ -11,14 +11,13 @@ module Cloud.AWS.EC2.Snapshot
     ) where
 
 import Data.Text (Text)
-import Data.XML.Types (Event)
 import Data.Conduit
 import Control.Applicative
 
 import Cloud.AWS.EC2.Internal
 import Cloud.AWS.EC2.Types
 import Cloud.AWS.EC2.Query
-import Cloud.AWS.Lib.Parser
+import Cloud.AWS.Lib.Parser.Unordered
 
 describeSnapshots
     :: (MonadResource m, MonadBaseControl IO m)
@@ -29,7 +28,7 @@ describeSnapshots
     -> EC2 m (ResumableSource m Snapshot)
 describeSnapshots ssids owners restby filters =
     ec2QuerySource "DescribeSnapshots" params $
-        itemConduit "snapshotSet" snapshotSink
+        itemConduit' "snapshotSet" snapshotConv
   where
     params =
         [ "SnapshotId" |.#= ssids
@@ -38,19 +37,19 @@ describeSnapshots ssids owners restby filters =
         , filtersParam filters
         ]
 
-snapshotSink :: MonadThrow m
-    => Consumer Event m Snapshot
-snapshotSink = Snapshot
-        <$> getT "snapshotId"
-        <*> getT "volumeId"
-        <*> getT "status"
-        <*> getT "startTime"
-        <*> getT "progress"
-        <*> getT "ownerId"
-        <*> getT "volumeSize"
-        <*> getT "description"
-        <*> getT "ownerAlias"
-        <*> resourceTagSink
+snapshotConv :: (MonadThrow m, Applicative m)
+    => SimpleXML -> m Snapshot
+snapshotConv xml = Snapshot
+        <$> xml .< "snapshotId"
+        <*> xml .< "volumeId"
+        <*> xml .< "status"
+        <*> xml .< "startTime"
+        <*> xml .< "progress"
+        <*> xml .< "ownerId"
+        <*> xml .< "volumeSize"
+        <*> xml .< "description"
+        <*> xml .< "ownerAlias"
+        <*> resourceTagConv xml
 
 createSnapshot
     :: (MonadResource m, MonadBaseControl IO m)
@@ -58,7 +57,7 @@ createSnapshot
     -> Maybe Text -- ^ Description
     -> EC2 m Snapshot
 createSnapshot volid desc =
-    ec2Query "CreateSnapshot" params snapshotSink
+    ec2Query "CreateSnapshot" params $ xmlParser snapshotConv
   where
     params =
         [ "VolumeId" |= volid
@@ -70,7 +69,7 @@ deleteSnapshot
     => Text -- ^ SnapshotId
     -> EC2 m Bool
 deleteSnapshot ssid =
-    ec2Query "DeleteSnapshot" params $ getT "return"
+    ec2Query "DeleteSnapshot" params $ xmlParser (.< "return")
   where
     params = ["SnapshotId" |= ssid]
 
@@ -81,7 +80,7 @@ copySnapshot
     -> Maybe Text -- ^ Description
     -> EC2 m Text
 copySnapshot region sid desc =
-    ec2Query "CopySnapshot" params $ getT "snapshotId"
+    ec2Query "CopySnapshot" params $ xmlParser (.< "snapshotId")
   where
     params = [ "SourceRegion" |= region
              , "SourceSnapshotId" |= sid
@@ -94,27 +93,28 @@ describeSnapshotAttribute
     -> SnapshotAttributeRequest -- ^ Attribute
     -> EC2 m SnapshotAttribute
 describeSnapshotAttribute ssid attr =
-    ec2Query "DescribeSnapshotAttribute" params snapshotAttributeSink
+    ec2Query "DescribeSnapshotAttribute" params $
+        xmlParser snapshotAttributeConv
   where
     params =
         [ "SnapshotId" |= ssid
         , "Attribute" |= attr
         ]
 
-snapshotAttributeSink
-    :: MonadThrow m
-    => Consumer Event m SnapshotAttribute
-snapshotAttributeSink = SnapshotAttribute
-    <$> getT "snapshotId"
-    <*> itemsSet "createVolumePermission" createVolumePermissionItemSink
-    <*> productCodeSink
+snapshotAttributeConv
+    :: (MonadThrow m, Applicative m)
+    => SimpleXML -> m SnapshotAttribute
+snapshotAttributeConv xml = SnapshotAttribute
+    <$> xml .< "snapshotId"
+    <*> itemsSet' xml "createVolumePermission" createVolumePermissionItemConv
+    <*> productCodeConv xml
 
-createVolumePermissionItemSink :: MonadThrow m => Consumer Event m CreateVolumePermissionItem
-createVolumePermissionItemSink = do
-    mg <- elementM "group" text
+createVolumePermissionItemConv :: (MonadThrow m, Applicative m) => SimpleXML -> m CreateVolumePermissionItem
+createVolumePermissionItemConv xml = do
+    mg <- getElementM xml "group" content
     case mg of
         Just g -> return $ CreateVolumePermissionItemGroup g
-        Nothing -> CreateVolumePermissionItemUserId <$> getT "userId"
+        Nothing -> CreateVolumePermissionItemUserId <$> xml .< "userId"
 
 modifySnapshotAttribute
     :: (MonadResource m, MonadBaseControl IO m)
@@ -122,7 +122,7 @@ modifySnapshotAttribute
     -> CreateVolumePermission -- ^ CreateVolumePermission
     -> EC2 m Bool
 modifySnapshotAttribute ssid cvp =
-    ec2Query "ModifySnapshotAttribute" params $ getT "return"
+    ec2Query "ModifySnapshotAttribute" params $ xmlParser (.< "return")
   where
     params =
         [ "SnapshotId" |= ssid
@@ -146,7 +146,7 @@ resetSnapshotAttribute
     -> ResetSnapshotAttributeRequest -- ^ Attribute
     -> EC2 m Bool
 resetSnapshotAttribute ssid attr =
-    ec2Query "ResetSnapshotAttribute" params $ getT "return"
+    ec2Query "ResetSnapshotAttribute" params $ xmlParser (.< "return")
   where
     params =
         [ "SnapshotId" |= ssid
